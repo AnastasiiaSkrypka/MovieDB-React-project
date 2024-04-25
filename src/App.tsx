4aaf95569ef40d7960c2c5dfb7140933cef25758
YaroslavKyba <narxoc.ne@gmail.com>
2024-04-25 23:54:58 +0300

    Now Playing API, styles, Material UI 4

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+import { Link as RouterLink, Outlet } from "react-router-dom";
+import {
+  AppBar,
+  CssBaseline,
+  Link,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  createTheme,
+} from "@mui/material";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import { teal } from "@mui/material/colors";
+
+function HeaderLink({
+  children,
+  to,
+}: {
+  to: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <Link
+      component={RouterLink}
+      to={to}
+      variant="button"
+      color="inherit"
+      sx={{ my: 1, mx: 1.5 }}
+    >
+      {children}
+    </Link>
+  );
+}
+
+const defaultTheme = createTheme({
+  palette: {
+    primary: teal,
+    secondary: {
+      main: "#96000f",
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <LiveTvOutlinedIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            The Movies DB
+          </Typography>
+          <nav>
+            <HeaderLink to="/">Home</HeaderLink>
+            <HeaderLink to="/about">About</HeaderLink>
+            <HeaderLink to="/movies">Movies</HeaderLink>
+          </nav>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Outlet />
+      </main>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/store.ts
@@ -0,0 +1,20 @@
+import { composeWithDevTools } from "@redux-devtools/extension";
+import { UnknownAction, applyMiddleware, createStore } from "redux";
+import { ThunkAction, thunk } from "redux-thunk";
+import rootReducer from "./reducers";
+
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunk));
+const store = createStore(rootReducer, composedEnhancer);
+
+export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk<ReturnType> = ThunkAction<
+  ReturnType,
+  RootState,
+  undefined,
+  UnknownAction
+>;
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
